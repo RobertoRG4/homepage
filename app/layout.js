4066a21f96c3a89a90b79cd6675b397330c12684
RobertoRG4 <robertoromeroguadarrama@gmail.com>
2025-01-04 19:08:49 -0600

    adding SpeedInsights

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import Provider from "../components/provider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 export const metadata = {
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body suppressHydrationWarning={true}>
+        <SpeedInsights />
         <Provider>{children}</Provider>
       </body>
     </html>
